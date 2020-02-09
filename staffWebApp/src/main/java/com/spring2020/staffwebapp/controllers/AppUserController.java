6f841d6b44acfedece81ee8f0d83dbfe9927f529
Hoang Pham <hoangpham1.4.99@gmail.com>
2020-02-09 10:08:32 +0700

    WIP: refactoring packages

---
+++ staffWebApp/src/main/java/com/spring2020/staffwebapp/controllers/AppUserController.java
@@ -0,0 +1,18 @@
+package com.spring2020.staffwebapp.controllers;
+
+import com.spring2020.staffwebapp.domain.dto.DbResponseDto;
+import com.spring2020.staffwebapp.domain.entity.AppUser;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/appUser")
+public class AppUserController
+{
+    @PostMapping("/createUser")
+    public DbResponseDto createUser(AppUser appUser)
+    {
+        return null;
+    }
+}
